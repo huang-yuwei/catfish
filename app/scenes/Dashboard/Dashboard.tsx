4ab12d95283313029d73b04a9dd5282a9123eaa7
yuwei huang <yuwei.huang@dena.com>
2022-05-26 17:09:38 +0900

    :tada: render the google map via the google maps API

--- app/scenes/Dashboard/Dashboard.tsx
+++ app/scenes/Dashboard/Dashboard.tsx
@@ -1,9 +1,17 @@
-import { Box } from './Dashboard.style';
+import { Wrapper as MapinIntiator } from '@googlemaps/react-wrapper';
+import { useLoaderData } from '@remix-run/react';
+import { Map } from './components/Map';
+import type { MapApiKey } from './Dashboard.server';
+import { Layout } from './Dashboard.style';
 
 export const Dashboard = () => {
+  const apiKey = useLoaderData<MapApiKey>();
+
   return (
-    <Box>
-      <h1>Welcome to Dashboard</h1>
-    </Box>
+    <Layout>
+      <MapinIntiator apiKey={apiKey}>
+        <Map />
+      </MapinIntiator>
+    </Layout>
   );
 };
